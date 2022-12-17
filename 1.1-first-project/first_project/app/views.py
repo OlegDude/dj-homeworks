3d768441e2e459b5d61ec654a514d9e9bf4befdd
Oleg Bondarev <po4ta_oleg@list.ru>
2022-12-17 23:43:12 +0300

    Deleted comments

--- 1.1-first-project/first_project/app/views.py
+++ 1.1-first-project/first_project/app/views.py
@@ -6,16 +6,12 @@ from django.shortcuts import render, reverse
 
 def home_view(request):
     template_name = 'app/home.html'
-    # впишите правильные адреса страниц, используя
-    # функцию `reverse`
     pages = {
         'Главная страница': reverse('home'),
-        'Показать текущее время': reverse('current_time'),
+        'Показать текущее время': reverse('time'),
         'Показать содержимое рабочей директории': reverse('workdir')
     }
-    
-    # context и параметры render менять не нужно
-    # подбробнее о них мы поговорим на следующих лекциях
+
     context = {
         'pages': pages
     }
@@ -23,8 +19,6 @@ def home_view(request):
 
 
 def time_view(request):
-    # обратите внимание – здесь HTML шаблона нет, 
-    # возвращается просто текст
     current_time = datetime.datetime.now().time()
     str_time = current_time.strftime('%H:%M:%S')
     msg = f'Текущее время: {str_time}'
@@ -32,9 +26,6 @@ def time_view(request):
 
 
 def workdir_view(request):
-    # по аналогии с `time_view`, напишите код,
-    # который возвращает список файлов в рабочей 
-    # директории
     list_dir = os.listdir(path='.')
     msg = f'Список файлов в рабочей директории: {list_dir}'
     return HttpResponse(msg)
--- 1.1-first-project/first_project/first_project/urls.py
+++ 1.1-first-project/first_project/first_project/urls.py
@@ -20,11 +20,7 @@ from app.views import home_view, time_view, workdir_view
 
 urlpatterns = [
     path('', home_view, name='home'),
-    # Раскомментируйте код, чтобы данные урлы 
-    # обрабатывались Django
-    # path('current_time/', time_view, name='time'),
-    # path('workdir/', workdir_view, name='workdir'),
+    path('current_time/', time_view, name='time'),
+    path('workdir/', workdir_view, name='workdir'),
     path('admin/', admin.site.urls),
-    path('current_time/', time_view, name='current_time'),
-    path('workdir/', workdir_view, name='workdir')
 ]
