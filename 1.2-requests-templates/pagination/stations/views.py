37c916773c5d48fcc49debae7e05bc09d8821f4e
Oleg Bondarev <po4ta_oleg@list.ru>
2022-12-21 22:28:41 +0300

    deleted comments to the code

--- 1.2-requests-templates/pagination/stations/views.py
+++ 1.2-requests-templates/pagination/stations/views.py
@@ -1,5 +1,8 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render, redirect
 from django.urls import reverse
+from pagination import settings
+import csv
 
 
 def index(request):
@@ -7,11 +10,14 @@ def index(request):
 
 
 def bus_stations(request):
-    # получите текущую страницу и передайте ее в контекст
-    # также передайте в контекст список станций на странице
-
+    with open(settings.BUS_STATION_CSV, encoding='utf-8') as csvfile:
+        reader = list(csv.DictReader(csvfile))
+    page_number = int(request.GET.get("page", 1))
+    paginator = Paginator(reader, 10)
+    page = paginator.get_page(page_number)
+    list_stations_page = paginator.page(page_number)
     context = {
-    #     'bus_stations': ...,
-    #     'page': ...,
+         'bus_stations': list_stations_page,
+         'page': page,
     }
     return render(request, 'stations/index.html', context)
