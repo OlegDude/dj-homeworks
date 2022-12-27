17d6566b9133a30e310741d788b312918aec99c9
Oleg Bondarev <po4ta_oleg@list.ru>
2022-12-27 22:29:29 +0300

    27.12.2022 first commit

--- 2.1-databases/models_list_displaying/books/views.py
+++ 2.1-databases/models_list_displaying/books/views.py
@@ -1,7 +1,37 @@
 from django.shortcuts import render
+from books.models import Book
+from django.core.paginator import Paginator
+from datetime import datetime
 
 
-def books_view(request):
+def catalog(request):
     template = 'books/books_list.html'
-    context = {}
+    books = Book.objects.all()
+    context = {'books': books}
+    return render(request, template, context)
+
+
+def books_view(request, pub_date: datetime):
+    template = 'books/books_list.html'
+    books = Book.objects.filter(pub_date=pub_date)
+    prev_date = Book.objects.filter(pub_date__lt=pub_date).values('pub_date').first()
+    next_date = Book.objects.filter(pub_date__gt=pub_date).values('pub_date').first()
+    paginator = Paginator(books, 10)
+    page = paginator.get_page(pub_date)
+    if not prev_date:
+        context = {
+            'books': page,
+            'next': str(next_date['pub_date']),
+        }
+    elif not next_date:
+        context = {
+            'books': page,
+            'prev': str(prev_date['pub_date']),
+        }
+    else:
+        context = {
+            'books': page,
+            'prev': str(prev_date['pub_date']),
+            'next': str(next_date['pub_date']),
+        }
     return render(request, template, context)
--- 2.1-databases/models_list_displaying/main/urls.py
+++ 2.1-databases/models_list_displaying/main/urls.py
@@ -15,10 +15,27 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from datetime import datetime
 
-from books.views import books_view
+from books.views import books_view, catalog
+from django.urls import register_converter
+
+
+class DateConverter:
+    regex = r'[0-9]{4}-[0-9]{2}-[0-9]{2}'
+    format = '%Y-%m-%d'
+
+    def to_python(self, value: str) -> datetime:
+        return datetime.strptime(value, self.format)
+
+    def to_url(self, value: datetime) -> str:
+        return value.strftime(self.format)
+
+
+register_converter(DateConverter, 'date')
 
 urlpatterns = [
-    path('', books_view, name='books'),
+    path('books/', catalog, name='books'),
+    path('books/<date:pub_date>/', books_view),
     path('admin/', admin.site.urls),
 ]
