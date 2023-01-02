c10f41ee63f5dc1b15b352d07b3bc7b7328dd29b
Oleg Bondarev <po4ta_oleg@list.ru>
2023-01-02 22:18:57 +0300

    comment deleted

--- 2.2-databases-2/m2m-relations/articles/views.py
+++ 2.2-databases-2/m2m-relations/articles/views.py
@@ -9,9 +9,4 @@ def articles_list(request):
     context = {
         'object_list': articles
     }
-
-    # используйте этот параметр для упорядочивания результатов
-    # https://docs.djangoproject.com/en/3.1/ref/models/querysets/#django.db.models.query.QuerySet.order_by
-    ordering = '-published_at'
-
     return render(request, template, context)
