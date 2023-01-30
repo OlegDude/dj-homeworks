e1978cf171d3e09d21e7b8d8a3241f4b6381be88
Oleg Bondarev <po4ta_oleg@list.ru>
2023-01-31 00:41:48 +0300

    Without favorites

---
+++ 3.3-permissions/api_with_restrictions/advertisements/permissions.py
@@ -0,0 +1,25 @@
+from rest_framework import permissions
+
+
+class IsAdminOwnerOrReadOnly(permissions.BasePermission):
+
+    def has_object_permission(self, request, view, obj):
+
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        elif request.user and request.user.is_staff:
+            return True
+
+        return obj.creator.id == request.user.id
+
+
+class IsOwnerReadDraft(permissions.BasePermission):
+
+    def has_object_permission(self, request, view, obj):
+        print(request)
+        if request.method in permissions.SAFE_METHODS:
+            if obj.status == 'DRAFT':
+                return obj.creator.id == request.user.id or request.user.is_staff
+
+            return True
+
--- 3.3-permissions/api_with_restrictions/advertisements/views.py
+++ 3.3-permissions/api_with_restrictions/advertisements/views.py
@@ -5,7 +5,7 @@ from rest_framework import filters
 
 from advertisements.filters import AdvertisementFilter
 from advertisements.models import Advertisement
-from advertisements.permissions import IsAdminOwnerOrReadOnly
+from advertisements.permissions import IsAdminOwnerOrReadOnly, IsOwnerReadDraft
 from advertisements.serializers import AdvertisementSerializer
 
 
@@ -20,7 +20,9 @@ class AdvertisementViewSet(ModelViewSet):
 
     def get_permissions(self):
         """Получение прав для действий."""
-        if self.action in ["create"]:
+        if self.action == "retrieve":
+            return [IsOwnerReadDraft()]
+        elif self.action in ["create"]:
             return [IsAuthenticated()]
         elif self.action in ["destroy", "update", "partial_update"]:
             return [IsAdminOwnerOrReadOnly()]
